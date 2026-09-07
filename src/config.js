c1475580d7e97026fad221be56a0d94ebea270e3
NZTL <tomrcoffey2007@gmail.com>
2026-09-07 12:40:45 +1000

    Keep recently active wallets on V3 hot polling

--- src/config.js
+++ src/config.js
@@ -27,6 +27,7 @@ export const config = {
   // every few seconds.
   walletSchedulerMs: numberEnv('WALLET_SCHEDULER_MS', 2000, 1000),
   walletHotPollMs: numberEnv('WALLET_HOT_POLL_MS', 15000, 5000),
+  walletHotHoldMs: numberEnv('WALLET_HOT_HOLD_MS', 300000, 30000),
   walletIdlePollMs: numberEnv('WALLET_IDLE_POLL_MS', 60000, 10000),
   pricePollMs: numberEnv('PRICE_POLL_MS', 30000, 10000),
   recentTxLimit: Math.floor(numberEnv('HELIUS_TX_LIMIT', 20, 5)),
